aa5b39bd726b6085437b40221d9009033ac217a8
Sean Genabe <seangenabe@outlook.com>
2016-03-29 16:52:39 +0800

    add composite-error for pinpointing require errors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var globby = require('globby')
 var Path = require('path')
+var CompositeError = require('composite-error')
+var Util = require('util')
 
 module.exports = function requireGlobArray(patterns, opts) {
   if (typeof patterns === 'object' && !Array.isArray(patterns) &&
@@ -16,6 +18,23 @@ module.exports = function requireGlobArray(patterns, opts) {
   }
   return globby.sync(patterns, opts)
     .map(function(path) {
-      return require(Path.join(opts.cwd || process.cwd(), path))
+      try {
+        let realpath = Path.join(opts.cwd || process.cwd(), path)
+        return require(realpath)
+      }
+      catch (err) {
+        throw new RequireError(
+          "An error occurred while trying to require the file " + realpath,
+          err
+        )
+      }
     })
 }
+
+function RequireError(realpath, message, innerErrors) {
+  CompositeError.call(this, message, innerErrors)
+  this.path = realpath
+  this.name = 'RequireError'
+}
+
+Util.inherits(RequireError, CompositeError)
